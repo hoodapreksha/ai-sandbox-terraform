c6cb3bb241efdc66f91f5ede12df807ef4b77279
Preksha Hooda <hooda.preksha@gmail.com>
2025-11-20 09:40:32 -0500

    resource-group-creation

--- envs/sandbox/main.tf
+++ envs/sandbox/main.tf
@@ -3,12 +3,25 @@ terraform {
 
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 4.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "054ba805-4e5d-4cad-a706-02c6eb02c567"
+}
+
+resource "azurerm_resource_group" "sandbox" {
+  name     = "rg-ai-sandbox-sandbox"
+  location = "canadacentral"
+
+  tags = {
+    environment = "sandbox"
+    project     = "ai-sandbox"
+    owner       = "preksha"
+    managed_by  = "terraform"
+  }
 }
